c91d4da6bc9131b602ea65f019113aef7761ba9c
Rohit Rana <rkrana6631@gmail.com>
2023-07-09 20:33:33 +0530

    done

--- routes/auth.js
+++ routes/auth.js
@@ -17,6 +17,7 @@ router.post("/register", async (req, res) => {
       name:req.body.name,
       username: req.body.username,
       email: req.body.email,
+      specification: req.body.specification,
       password: hashedPassword,
     });
 
